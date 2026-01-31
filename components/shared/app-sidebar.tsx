062561a94d6978f2a40b3b14206e706059a27da4
Mikolaj <rudkowski.dev@gmail.com>
2026-01-31 22:20:39 +0100

    design changes

--- components/shared/app-sidebar.tsx
+++ components/shared/app-sidebar.tsx
@@ -23,8 +23,8 @@ const items = [
 		icon: Calendar,
 	},
 	{
-		title: 'Excercises',
-		url: '/dashboard/excercises',
+		title: 'Exercises',
+		url: '/dashboard/exercises',
 		icon: Dumbbell,
 	},
 	{
